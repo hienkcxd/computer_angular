db79bfb959ab0802cb77c0f20f554fe500ed1c07
hienkcxd <70784911+hienkcxd@users.noreply.github.com>
2023-07-28 14:11:34 +0700

    update module

--- src/app/module/user/user.module.ts
+++ src/app/module/user/user.module.ts
@@ -6,6 +6,12 @@ import {HomepageModule} from "./homepage/homepage.module";
 import {ShopModule} from "./shop/shop.module";
 import {CpuModule} from "./cpu/cpu.module";
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {CoolerModule} from "./cooler/cooler.module";
+import {MonitorModule} from "./monitor/monitor.module";
+import {RamModule} from "./ram/ram.module";
+import {VgaModule} from "./vga/vga.module";
+import {VoltageModule} from "./voltage/voltage.module";
+import {MouseModule} from "./mouse/mouse.module";
 
 
 
@@ -17,8 +23,14 @@ import {RouterLink, RouterOutlet} from "@angular/router";
     CommonModule,
     LayoutModule,
     HomepageModule,
-    CpuModule,
     ShopModule,
+    CoolerModule,
+    CpuModule,
+    MonitorModule,
+    MouseModule,
+    RamModule,
+    VgaModule,
+    VoltageModule,
     RouterOutlet,
     RouterLink,
     UserRoutingModule
